1e3a0593feeb43733d0f5cdfb73c439e21a5e767
Shawn <shawnkimwk@gmail.com>
2021-10-08 08:22:58 +0900

    edited css

--- src/home/HomePopUp.js
+++ src/home/HomePopUp.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Popup from 'reactjs-popup'
 import "./HomePopUp.css"
 
-const HomePopUp = ({open, onClose, url, title}) => {
+const HomePopUp = ({open, onClose, url, title, ref}) => {
   return (
     <Popup open={open} closeOnDocumentClick onClose={onClose}>
       <div className="modal">
-        <a className="close" onClick={onClose}>
+        <button className="close" onClick={onClose}>
           &times;
-        </a>
+        </button>
         <iframe title={title} src={url}></iframe>
       </div>
     </Popup>
