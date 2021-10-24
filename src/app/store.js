04ec88a58aa6339d19ffc0f148fa21e4274c94e4
Shawn <shawnkimwk@gmail.com>
2021-10-24 22:23:25 +0900

    edited redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
 
-export const store = configureStore({
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if(serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    // Ignore write errors;
+  }
+};
+
+const persistedState = loadState();
+
+const store = configureStore({
+  persistedState,
+  // Others reducers...
   reducer: {
     user: userReducer
   },
 });
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export {store}
