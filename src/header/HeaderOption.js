d283d83d6571d6d85728e5c9d9bc631370a23b1c
Shawn <shawnkimwk@gmail.com>
2021-10-28 22:51:06 +0900

    edited sidebar

--- src/header/HeaderOption.js
+++ src/header/HeaderOption.js
@@ -4,9 +4,9 @@ import "./HeaderOption.css"
 
 function HeaderOption({ Icon, title, onClick, isToggled }) {
   return (
-    <div onClick={onClick} className="headerOption">
+    <div onClick={onClick} className={`headerOption ${isToggled && 'toggled'}`}>
       {Icon && <Icon className="headerOption__icon" />}
-      <span className="headerOption__title">{isToggled ? title : ''}</span>
+      <span className={`headerOption__title ${isToggled && 'toggled'}`}>{title}</span>
     </div>
   )
 }
