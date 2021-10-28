656e3614b915833bb0a6e66a03b34a4a181e5953
Shawn <shawnkimwk@gmail.com>
2021-10-29 00:40:09 +0900

    edited userprofile

--- src/userProfile/UserProfile.js
+++ src/userProfile/UserProfile.js
@@ -19,7 +19,8 @@ const UserProfile = ({ isToggled }) => {
   const userDisplayName = useSelector(selectDisplayName);
   const userProfileURL = useSelector(selectProfileURL);
   const [phoneNum, setPhoneNum] = useState("");
-  const [hobby, setHobby] = useState("");
+  const [likes, setLikes] = useState("");
+  const [dislikes, setDislikes] = useState("");
   const [error, setError] = useState("");
   const [userData, setUserData] = useState([]);
   const [isActive, setIsActive] = useState(false);
@@ -39,7 +40,8 @@ const UserProfile = ({ isToggled }) => {
           name: userDisplayName,
           email: userEmail,
           phoneNum: "",
-          hobby: "",
+          likes: "",
+          dislikes: "",
           photoUrl: userProfileURL || "",
     
         }).then(() => {
@@ -67,7 +69,8 @@ const UserProfile = ({ isToggled }) => {
       name: userDisplayName,
       email: userEmail,
       phoneNum: (phoneNum && isValidPhoneNumber(phoneNum)) ? phoneNum : (userData.phoneNum ? userData.phoneNum : ""),
-      hobby: hobby.length > 0 ? hobby : (userData.hobby ? userData.hobby : ""),
+      likes: likes.length > 0 ? likes : (userData.likes ? userData.likes : ""),
+      dislikes: dislikes.length > 0 ? dislikes : (userData.dislikes ? userData.dislikes : ""),
       photoUrl: userProfileURL || "",
 
     }).then(() => {
@@ -82,9 +85,10 @@ const UserProfile = ({ isToggled }) => {
     );
 
     setPhoneNum(undefined);
-    setHobby("");
+    setLikes("");
+    setDislikes("");
     if (phoneNum && !isValidPhoneNumber(phoneNum)) setError("Invalid Phone Number");
-    if (!phoneNum && hobby.length === 0) setError("No Profile Info To Update");
+    if (!phoneNum && likes.length === 0 && dislikes.length === 0) setError("No Profile Info To Update");
   }
 
   return (
@@ -99,7 +103,8 @@ const UserProfile = ({ isToggled }) => {
               <div>Name: <span>{userData.name}</span></div>
               <div>Email: <span>{userData.email}</span></div>
               <div>Phone Number: <span>{userData.phoneNum}</span></div>
-              <div>Hobby: <span>{userData.hobby}</span></div>
+              <div>Likes: <span>{userData.likes}</span></div>
+              <div>Dislikes: <span>{userData.dislikes}</span></div>
               <button className="btn" onClick={() => setIsActive(!isActive)}>Update</button>
             </div>
             <div className="imgBx"><img src={ImageTwo} alt="locked" /></div>
@@ -113,7 +118,8 @@ const UserProfile = ({ isToggled }) => {
                 <input style={{border: '1px solid crimson'}} placeholder={userDisplayName} type="text" readOnly/>
                 <input style={{border: '1px solid crimson'}} placeholder={userEmail} type="text" readOnly/>
                 <PhoneInput onChange={setPhoneNum} value={phoneNum} placeholder="Phone Number"/>
-                <input onChange={(e) => setHobby(e.target.value)} placeholder="Hobby" type="text"/>
+                <input onChange={(e) => setLikes(e.target.value)} placeholder="Likes" type="text"/>
+                <input onChange={(e) => setDislikes(e.target.value)} placeholder="DisLikes" type="text"/>
                 <div className="btnBx">
                   <button type="submit" className="btn" onClick={() => setIsActive(!isActive)}>Save</button>
                 </div>
