983ab1cad8e6dc8e4235cddd39b3fccda560bcb8
Shawn <shawnkimwk@gmail.com>
2021-10-30 19:42:56 +0900

    edited home

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+import Particles from 'react-tsparticles'
+import { params } from './Params.js'
+
 import './Home.css'
 
 const Home = ({isToggled}) => {
   return (
     <div className={`home_content ${isToggled && 'toggled'}`}>
-      <div className="home">Home Content</div>
+      <Particles params={params} />
+
+      <div className="home">
+        <h1>Home Content</h1>
+        <div>Hello world</div>
+      </div>
     </div>
   )
 }
